e10d48470c919ffe7e3e3cc6fc326f0c4d96c506
Lncvrt <lncvrtreal@gmail.com>
2025-08-17 10:32:16 -0700

    Fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout ({
 }>) {
   return (
     <html lang='en'>
-      <body className={lexend.className}>{children}</body>
+      <body className={lexend.className}>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
