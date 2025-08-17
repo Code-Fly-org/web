4b22d31989bcbe7d70d2df5c8e2ef70d1ebb082a
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-17 11:42:01 -0700

    Added the logo to the website
    It was pretty annoying to make it look good but I did it yay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next'
+import Image from 'next/image'
+import codefly from './assets/logo.png'
 
 export const metadata: Metadata = {
   title: 'Code Fly - Home'
@@ -7,9 +9,15 @@ export const metadata: Metadata = {
 export default function Home () {
   return (
     <>
-      <div className="side-gradient">
+      <div className='side-gradient'>
         <h1 className='side-text'>Code Fly is a suite of IDE&apos;s</h1>
-        <h1 className='side-text'>Made as a free alternative to the jetbrains suite</h1>
+        <h1 className='side-text'>
+          Made as a free alternative to the jetbrains suite
+        </h1>
+      </div>
+
+      <div className='logo-container'>
+        <Image src={codefly} alt='Code Fly Logo' width={600} height={600} />
       </div>
     </>
   )
