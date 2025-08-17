09c040154cab5bdf2afe052352855af13da79621
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-17 11:07:02 -0700

    Fixed a stupid error
    whyyyyy ahahahha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home () {
   return (
     <>
       <div className="side-gradient">
-        <h1 className='side-text'>Code Fly is a suite of IDE's</h1>
+        <h1 className='side-text'>Code Fly is a suite of IDE&apos;s</h1>
         <h1 className='side-text'>Made as a free alternative to the jetbrains suite</h1>
       </div>
     </>
