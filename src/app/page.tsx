7ab2c5ad28f59bf438542436dcb74a13641ec573
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-17 12:51:48 -0700

    Made the logo do a spin
    wtf is this commit name anyways the squares in the logo spin now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home () {
       </div>
 
       <div className='logo-container'>
-        <div className='bg-[#5331F8] w-100 h-100 rounded-2xl rotate-345'></div>
-        <div className='bg-[#ED46F8] w-100 h-100 rounded-2xl rotate-15'></div>
+        <div className='bg-[#5331F8] w-100 h-100 rounded-2xl rotate-345 [animation:spin_50s_linear_infinite]'></div>
+        <div className='bg-[#ED46F8] w-100 h-100 rounded-2xl rotate-15 [animation:spin_40s_linear_infinite_reverse]'></div>
         <div className='bg-[#000000] w-70 h-70 rounded-2xl'>
           <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !text-[#8937F8] text-5xl whitespace-nowrap'>
             Code Fly
