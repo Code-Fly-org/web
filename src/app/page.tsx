6ac9de87e5eb7636b652ac303f640b8e9b7716db
Lncvrt <lncvrtreal@gmail.com>
2025-08-17 12:19:29 -0700

    Fix colors in logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,12 @@ export default function Home () {
       </div>
 
       <div className='logo-container'>
-        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#4400ff] w-100 h-100 rounded-2xl rotate-345'></div>
-        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#ea00ff] w-100 h-100 rounded-2xl rotate-15'></div>
+        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#5331F8] w-100 h-100 rounded-2xl rotate-345'></div>
+        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#ED46F8] w-100 h-100 rounded-2xl rotate-15'></div>
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#000000] w-70 h-70 rounded-2xl'>
-          <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !text-[#9900ff] text-5xl whitespace-nowrap'>Code Fly</h1>
+          <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !text-[#8937F8] text-5xl whitespace-nowrap'>
+            Code Fly
+          </h1>
         </div>
       </div>
     </>
