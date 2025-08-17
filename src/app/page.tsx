a9c3f24cd51a21c31de799688c1efce3a511a6c0
Lncvrt <lncvrtreal@gmail.com>
2025-08-17 10:18:14 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Code Fly - Home'
+}
+
+export default function Home () {
+  return (
+    <>
+      <p>Code Fly Website</p>
+    </>
+  )
+}
