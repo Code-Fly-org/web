4912d5cdcc5a50b6579cb84c7bfce62bb1585da8
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-18 17:44:13 -0700

    Added links
    I wanna make the gradient change color when you scroll but I am gonna do
    that later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,19 @@ export default function Home () {
   return (
     <>
       <div className='side-gradient'>
-        <h1 className='side-text'>Code Fly is a suite of IDE&apos;s</h1>
-        <h1 className='side-text'>
+        <h1 className='top-side-text'>Code Fly is a suite of IDE&apos;s</h1>
+        <h1 className='side-text-nm'>
           Made as a free alternative to the jetbrains suite
         </h1>
+
+        <h1 className='side-text'>
+          <a href="https://github.com/Code-Fly-org">Github</a>
+        </h1>
+        <h1 className='side-text-nm'>
+          <a href="https://discord.gg/7Y2vaedb3u">Discord</a>
+        </h1>
+
+        <div className='h-70'></div>
       </div>
 
       <div className='logo-container'>
