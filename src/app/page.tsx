e31f688d5ffd41aa10e5a42c98d5a1af2a1178e4
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-08-25 10:08:17 -0700

    Cleaned up some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,9 +24,9 @@ export default function Home () {
       </div>
 
       <div className='logo-container'>
-        <div className='bg-[#5331F8] w-100 h-100 rounded-2xl rotate-345 [animation:spin_50s_linear_infinite]'></div>
-        <div className='bg-[#ED46F8] w-100 h-100 rounded-2xl rotate-15 [animation:spin_40s_linear_infinite_reverse]'></div>
-        <div className='bg-[#000000] w-70 h-70 rounded-2xl'>
+        <div className='bg-[#5331F8] w-100 h-100 rotate-345 [animation:spin_50s_linear_infinite]'></div>
+        <div className='bg-[#ED46F8] w-100 h-100 rotate-15 [animation:spin_40s_linear_infinite_reverse]'></div>
+        <div className='bg-[#000000] w-70 h-70'>
           <h1 className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 !text-[#8937F8] text-5xl whitespace-nowrap'>
             Code Fly
           </h1>
